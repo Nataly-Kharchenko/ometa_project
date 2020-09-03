ac5853a2c332f9a37c3bf49e69f9f7e5281c0646
Nataly-Kharchenko <natalinatakla@gmail.com>
2020-09-03 18:43:54 +0300

    some new4

--- ometa/admin.py
+++ ometa/admin.py
@@ -3,13 +3,11 @@ from django.contrib import admin
 
 # Register your models here.
 
-
 from .models import Photographer, Album, Photo, Director, Video, Work, About_U, Contact, Addres, Preview_Video
 
 
 admin.site.register(Photographer)
-admin.site.register(Album)
-admin.site.register(Photo)
+
 admin.site.register(Director)
 admin.site.register(Video)
 admin.site.register(Work)
@@ -17,3 +15,21 @@ admin.site.register(About_U)
 admin.site.register(Contact)
 admin.site.register(Addres)
 admin.site.register(Preview_Video)
+
+
+class PhotoAdmin(admin.ModelAdmin):
+    pass
+
+
+class PhotoInline(admin.StackedInline):
+    model = Photo
+    max_num = 50
+    extra = 0
+
+
+class AlbumAdmin(admin.ModelAdmin):
+    inlines = [PhotoInline]
+
+
+admin.site.register(Album, AlbumAdmin)
+admin.site.register(Photo, PhotoAdmin)
